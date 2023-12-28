3bd8fa3cb591f32eb6286acffb9ec53fd403e891
maxeq <60312202+maxeq@users.noreply.github.com>
2023-12-28 15:57:09 +0100

    subgraph

--- src/game-marketplace.ts
+++ src/game-marketplace.ts
@@ -1,5 +1,5 @@
-import { Upgraded as UpgradedEvent } from "../generated/GameMarketplace/GameMarketplace"
-import { Upgraded } from "../generated/schema"
+import { Upgraded as UpgradedEvent, ItemBought as ItemBoughtEvent, ItemListed as ItemListedEvent, PriceChanged as PriceChangedEvent, ListingCancelled as ListingCancelledEvent } from "../generated/GameMarketplace/GameMarketplace"
+import { ItemBought, ItemListed, ListingCancelled, PriceChanged, Upgraded } from "../generated/schema"
 
 export function handleUpgraded(event: UpgradedEvent): void {
   let entity = new Upgraded(
@@ -13,3 +13,54 @@ export function handleUpgraded(event: UpgradedEvent): void {
 
   entity.save()
 }
+
+
+export function handleItemListed(event: ItemListedEvent): void {
+  let id = event.params.nftAddress.toHexString() + '-' + event.params.tokenId.toString();
+  let entity = new ItemListed(id);
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
+  entity.seller = event.params.seller;
+  entity.price = event.params.price;
+  entity.itemTokenIds = event.params.itemTokenIds;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.save();
+}
+
+
+export function handleListingCancelled(event: ListingCancelledEvent): void {
+  let id = event.params.nftAddress.toHexString() + '-' + event.params.tokenId.toString();
+  let entity = new ListingCancelled(id);
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.save();
+}
+
+
+export function handlePriceChanged(event: PriceChangedEvent): void {
+  let id = event.params.nftAddress.toHexString() + '-' + event.params.tokenId.toString();
+  let entity = new PriceChanged(id);
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
+  entity.newPrice = event.params.newPrice;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.save();
+}
+
+
+export function handleItemBought(event: ItemBoughtEvent): void {
+  let id = event.params.nftAddress.toHexString() + '-' + event.params.tokenId.toString();
+  let entity = new ItemBought(id);
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
+  entity.buyer = event.params.buyer;
+  entity.price = event.params.price;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.save();
+}
+
