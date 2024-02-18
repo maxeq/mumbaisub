5215e9590c315c329ad95b8d31cb0a7da9201685
maxeq <60312202+maxeq@users.noreply.github.com>
2024-02-18 13:39:45 +0100

    Update dungeon-adventure.ts

--- src/dungeon-adventure.ts
+++ src/dungeon-adventure.ts
@@ -1,4 +1,4 @@
-import { BigInt, log } from "@graphprotocol/graph-ts";
+import { BigInt } from "@graphprotocol/graph-ts";
 import { DungeonResult as DungeonResultEvent } from "../generated/DungeonAdventure/DungeonAdventure";
 import { CharacterDailySummary, DungeonResult } from "../generated/schema";
 
@@ -30,10 +30,11 @@ export function handleDungeonResult(event: DungeonResultEvent): void {
   entity.openSlots = event.params.openSlots;
 
   entity.save();
-
+  
+  let dayMonthYear = dayMonthYearFromEventTimestamp(event);
   let characterId = event.params.characterId.toString();
-  let date = event.block.timestamp;
-  let id_day = characterId + "-" + date.toString();
+  let formattedDate = dayMonthYear.year.toString() + "-" + dayMonthYear.month.toString().padStart(2, '0') + "-" + dayMonthYear.day.toString().padStart(2, '0');
+  let id_day = characterId + "-" + formattedDate;
 
   let dailySummary = CharacterDailySummary.load(id_day);
 
@@ -61,4 +62,40 @@ export function handleDungeonResult(event: DungeonResultEvent): void {
   dailySummary.totalExperiences = dailySummary.totalExperiences.plus(event.params.experience);
 
   dailySummary.save();
+}
+
+class DayMonthYear {
+  day: BigInt;
+  month: BigInt;
+  year: BigInt;
+
+  constructor(day: BigInt, month: BigInt, year: BigInt) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+}
+
+const SECONDS_IN_DAY: i32 = 86400; // Количество секунд в сутках
+const ZERO: BigInt = BigInt.fromI32(0);
+const ONE: BigInt = BigInt.fromI32(1);
+
+export function dayMonthYearFromEventTimestamp(event: DungeonResultEvent ): DayMonthYear {
+  let unixEpoch: BigInt = BigInt.fromI32(event.block.timestamp.toI32()); // Предполагаем, что timestamp уже в секундах
+
+  let daysSinceEpochStart: BigInt = unixEpoch.div(BigInt.fromI32(SECONDS_IN_DAY)).plus(BigInt.fromI32(719468));
+  
+  let era: BigInt = (daysSinceEpochStart.ge(ZERO) ? daysSinceEpochStart : daysSinceEpochStart.minus(BigInt.fromI32(146096))).div(BigInt.fromI32(146097));
+  let dayOfEra: BigInt = daysSinceEpochStart.minus(era.times(BigInt.fromI32(146097))); // [0, 146096]
+  let yearOfEra: BigInt = (dayOfEra.minus(dayOfEra.div(BigInt.fromI32(1460))).plus(dayOfEra.div(BigInt.fromI32(36524))).minus(dayOfEra.div(BigInt.fromI32(146096)))).div(BigInt.fromI32(365)); // [0, 399]
+  
+  let year: BigInt = yearOfEra.plus(era.times(BigInt.fromI32(400)));
+  let dayOfYear: BigInt = dayOfEra.minus((BigInt.fromI32(365).times(yearOfEra)).plus(yearOfEra.div(BigInt.fromI32(4))).minus(yearOfEra.div(BigInt.fromI32(100)))); // [0, 365]
+  let monthZeroIndexed: BigInt = (BigInt.fromI32(5).times(dayOfYear).plus(BigInt.fromI32(2))).div(BigInt.fromI32(153)); // [0, 11]
+  let day: BigInt = dayOfYear.minus((BigInt.fromI32(153).times(monthZeroIndexed).plus(BigInt.fromI32(2))).div(BigInt.fromI32(5))).plus(ONE); // [1, 31]
+  let month: BigInt = monthZeroIndexed.plus(monthZeroIndexed.lt(BigInt.fromI32(10)) ? BigInt.fromI32(3) : BigInt.fromI32(-9)); // [1, 12]
+
+  year = month.le(BigInt.fromI32(2)) ? year.plus(ONE) : year;
+  
+  return new DayMonthYear(day, month, year);
 }
